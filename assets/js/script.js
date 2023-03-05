6b8375959622f591b8023dacea61fae16fd9c83b
xybai0103 <xueyin0103@gmail.com>
2023-03-04 19:56:18 -0600

    add dayjs library, add eventlistener for searchButton, fetch geocoding API and currenWeather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,42 @@
 // an acquired API key to use OpenWeather API
 var APIKey = '5d2d2bb20c65ab0c48043cb7483bf366';
+// date format
+var today = dayjs();
+var date = today.format('M/DD/YYYY');
+console.log(date);
+// search city weather
+var searchBtn = '.btn search';
+// current weather
+var currentDate = $('#current-date');
+var currentTemp = $('#current-temp');
+var currentWind = $('#current-wind');
+var currentHumidity = $('#current-humidity');
 // to collect user input for the city name and store it in a variable
-var city;
-// use string concatenation to create the queryURL variable, which will store the
-// OpenWeather Current Weather Data URL and the necessary variables
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var city = $('#search-city').val();
+// Using Geocoding API, convert a city name into the exact geographical coordinates
+var queryGeoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + APIKey;
 
-// to call the Fetch API to pass the query URL in as a parameter
-fetch(queryURL)
+fetch(queryGeoURL)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+    var lat = data[0].lat;
+    var lon = data[0].lon;
+
+    // Call for current weather data
+    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey;
+
+    fetch(queryURL)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      console.log(data);
+      currentData.text(city + '(' + dayjs + ')')
+    });
+  });
+
+searchBtn.click(searchBtnHandler);
+
